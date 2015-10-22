fa0925ee7f9124d250a5f5f615e6fe5a1ebb0153
zackluckyf <fanning.34@gmail.com>
2015-10-22 18:26:44 -0400

    added

---
+++ ch13-exercises/ch13-exercises/brass.hpp
@@ -0,0 +1,49 @@
+//
+//  brass.hpp
+//  ch13-exercises
+//
+//  Created by Zack Fanning on 10/22/15.
+//  Copyright © 2015 Zackluckyf. All rights reserved.
+//
+
+#ifndef brass_hpp
+#define brass_hpp
+#include <string>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Brass
+{
+protected:
+    std::string fullName;
+    long acctNum;
+    double balance;
+public:
+    Brass(const std::string & s = "Nullbody", long an = -1, double bal = 0.0);
+    virtual void Withdraw(double amt);
+    double Balance() const;
+    virtual void ViewAcct() const;
+    virtual ~Brass() {};
+    void Deposit(double amt);
+};
+
+//Brass Plus Account Class
+class BrassPlus : public Brass
+{
+private:
+    double maxLoan;
+    double rate;
+    double owesBank;
+public:
+    BrassPlus(const std::string & s = "Nullbody", long an = -1, double bal = 0.0, double ml = 500, double r = 0.11125);
+    BrassPlus(const Brass & ba, double ml = 500, double r = 0.11125);
+    virtual void ViewAcct()const;
+    virtual void Withdraw(double amt);
+    void ResetMax(double m) { maxLoan = m; }
+    void ResetRate(double r) { rate = r; };
+    void ResetOwes() { owesBank = 0; }
+};
+
+#endif /* brass_hpp */
---
+++ ch13-exercises/ch13-exercises/ch13exercises.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  ch13-exercises
+//
+//  Created by Zack Fanning on 10/22/15.
+//  Copyright © 2015 Zackluckyf. All rights reserved.
+//
+
+#include <iostream>
+#include "brass.hpp"
+
+int main(int argc, const char * argv[])
+{
+    /*
+    Brass Harvey("Harvery Oswald", 123456789, 4183.45);
+    Brass Helga("Helga Horatio", 987654321, 2592.00);
+    BrassPlus Helga2(Helga, 500, .125);
+    Harvey.Deposit(100);
+    Harvey.ViewAcct();
+    Harvey.Withdraw(3000);
+    Harvey.Withdraw(1500);
+    Harvey.ViewAcct();
+    Helga2.Brass::Deposit(500);
+    Helga2.ViewAcct();
+    Helga2.BrassPlus::Withdraw(2500);
+    Helga2.ViewAcct();
+    Helga2.BrassPlus::Withdraw(1000);
+    Helga2.ViewAcct();
+     */
+    
+    
+    
+    
+    
+    return 0;
+}
